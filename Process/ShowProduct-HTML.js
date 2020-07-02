416aef9440139d47b0e4c421ef0cf98870fff4e6
hahadaubo <64895798+hahadaubo@users.noreply.github.com>
2020-07-02 21:14:18 +0700

    Finish

---
+++ Process/ShowProduct-HTML.js
@@ -0,0 +1,147 @@
+function DomID(id){
+    return document.getElementById(id);
+}
+function TangDan(){
+    var priceElement=document.getElementsByClassName('money');
+    for(let i=0;i<priceElement.length;i++){
+            for ( var j = i + 1; j < priceElement.length; j++){
+            //Bỏ chữ đ sau giá tiền
+            if (parseFloat(priceElement[i].innerText.replace('đ','')) > parseFloat(priceElement[j].innerText.replace('đ',''))){
+                var temp = priceElement[i].parentElement.innerHTML
+                priceElement[i].parentElement.innerHTML = priceElement[j].parentElement.innerHTML;
+                priceElement[j].parentElement.innerHTML = temp;
+            }
+            }
+    }
+}
+function GiamDan(){   var priceElement=document.getElementsByClassName('money');
+    for(let i=0;i<priceElement.length;i++){
+            for ( var j = i + 1; j < priceElement.length; j++){
+            //Bỏ chữ đ sau giá tiền
+            if (parseFloat(priceElement[i].innerText.replace('đ','')) < parseFloat(priceElement[j].innerText.replace('đ',''))){
+                var temp = priceElement[i].parentElement.innerHTML
+                priceElement[i].parentElement.innerHTML = priceElement[j].parentElement.innerHTML;
+                priceElement[j].parentElement.innerHTML = temp;
+            }
+            }
+    }
+}
+
+DomID('countStore').innerHTML=0;
+
+var product=document.getElementsByClassName('product-title');
+    var tongTien=0;
+    DomID('TotalMoney').innerHTML=tongTien+'đ';
+
+  var imgs=[];
+  var titles=[];
+  var prices=[];
+  var numbers=[];
+
+  for(let i=0;i<product.length;i++){
+        var button=document.getElementsByClassName('button-buy');
+        button[i].addEventListener('click',()=>{
+            var product=button[i].parentElement.parentElement;
+            var title=product.getElementsByClassName('product-title')[0].innerText;
+            var img=product.getElementsByTagName('img')[0].src;
+            var price=product.getElementsByClassName('money')[0].innerText;
+            var number=product.getElementsByClassName('product-nmber')[0].value;
+            var titleBuy=document.getElementsByClassName('buy-title');
+            console.log(title);
+            console.log(img);
+            console.log(price);
+            console.log(number);
+            tongTien +=parseInt(price.replace('đ',''));
+
+            if (tongTien < 1000){
+                DomID('TotalMoney').innerHTML = tongTien + '.000đ';
+            }
+            else
+            {
+                var tempTien = tongTien.toString();
+                var tongTienTrieu = "";
+                for (var j = 0 ; j < tempTien.length; j++){
+                    if (j == 1){
+                        tongTienTrieu += "." + tempTien[j];
+                        continue;
+                    }
+                    tongTienTrieu += tempTien[j];
+                }
+                DomID('TotalMoney').innerHTML = tongTienTrieu + '.000đ';
+            }
+
+            if (titleBuy.length > 0) {
+                let flag = false;
+                for (let i = 0; i < titleBuy.length; i++) {
+                    if (titleBuy[i].innerText === title) {
+                        flag = true;
+                        numbers[i]=Number(++numbers[i]);
+                    }
+                }
+                    if (flag===false) {
+                        titles.push(title);
+                        imgs.push(img);
+                        prices.push(price);
+                        numbers.push(number);
+
+                    }
+                }
+            else {
+                titles.push(title);
+                imgs.push(img);
+                prices.push(price);
+                numbers.push(number);
+            }
+
+            renderHTML();
+        })
+  }
+
+  function renderHTML() {
+    var count =0;
+     var sanpham = "";
+     for (let i = 0; i < titles.length; i++) {
+        sanpham+= '<li class="show-SanPham"><img src="'+imgs[i]+'"alt=><div class="buy-title"><h4>'+titles[i]+'</h4></div><input type="text" value="'+numbers[i]+'" class="qual-buy" disabled><div class="buy-price"><p>x '+prices[i]+'</p></div><a class="delete" onclick="xoahang('+i+ ')">x</a></li>';
+        count+=Number(numbers[i]);
+
+     }
+     //tongTien = Number(prices.replace('đ',''));
+     DomID('countStore').innerHTML=count;
+     DomID('showSanPham').innerHTML=sanpham;
+    }
+
+     function xoahang(n){    
+        var x=document.getElementsByClassName('buy-title');
+        tongTien= tongTien -  parseFloat(prices[n].replace('đ','')) * Number(numbers[n]);
+        DomID('TotalMoney').innerHTML = tongTien + '000đ';
+        if (tongTien < 1000){
+            DomID('TotalMoney').innerHTML = tongTien + '.000đ';
+        }
+        else
+        {
+            var tempTien = tongTien.toString();
+            var tongTienTrieu = "";
+            for (var j = 0 ; j < tempTien.length; j++){
+                if (j == 1){
+                    tongTienTrieu += "." + tempTien[j];
+                    continue;
+                }
+                tongTienTrieu += tempTien[j];
+            }
+            DomID('TotalMoney').innerHTML = tongTienTrieu + '.000đ';
+        }
+        if(tongTien===0){
+            DomID('TotalMoney').innerHTML = tongTien + 'đ';
+        }
+        for (let i = 0; i < x.length; i++) {
+                if (titles[i] === x[n].innerText) {
+                    console.log();
+                    titles.splice(i, 1);
+                    imgs.splice(i,1);
+                    prices.splice(i,1);    
+                    numbers.splice(i,1);                                                 
+                }
+            }
+
+            renderHTML();
+    }
--- Process/ShowProduct.js
+++ Process/ShowProduct.js
@@ -10,7 +10,56 @@ var Product = [
         name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
         brandName: 'REPRESENT',
         price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
+    },
+
+    { 
+      imgFront: './IMG/Top/black1.jpg', 
+      imgBack: './IMG/Top/black2.jpg', 
+      name: 'VINTAGE BLACK WAFFLE LOGO T-SHIRT',
+      brandName: 'REPRESENT',
+      price: 450000,
     }
+    
 ]
 
 render(Product);
@@ -22,15 +71,15 @@ function render(arrContainer){
     var str = "";
 
     for (let v of arrContainer) {
-      str += '<div class="card fade"><div class="card-front effect"><img src="' + v.imgFront + '" height="300px" width="300px"></div>' +
-        '<div class="card-front effect"><img src="' + v.imgBack + '" height="300px" width="300px"></div></div>' +
+      str += '<div class="card fade"><div class="card-effect"><div class="card-front effect"><img src="' + v.imgFront + '" height="300px" width="300px"></div>' +
+        '<div class="card-back effect"><img src="' + v.imgBack + '" height="300px" width="300px"></div></div>' +
       '<div class="card-title">' +
       '<div class="product-title"><a href="#">' + v.name + '</a></div></br>' +
       '<a href="HomePage.html">' + v.brandName + '</a>'
       '<p class="money">' + v.price + "₫" + '</p></div>'
       arrFilter.push(str);
     }
-
+    
     console.log(arrFilter);
     
     var renderHtml = arrFilter.join('');
